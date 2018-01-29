8c8e4baa5279f52c2af4ceac501ec5bad9c759b7
houyujiang <houyujiang@live.cn>
2018-01-29 17:59:35 +0800

    成功读取串口数据

--- Com_Read_Test_py3.py
+++ Com_Read_Test_py3.py
@@ -13,21 +13,7 @@
 """
 import serial
 import re
-
-def read_Com():
-    serials = serial.Serial('Com3',115200)
-    argv = serials.read(40)
-    argv = ''
-    argv = argv.encode('utf-8')
-    print(argv)
-    print(type(argv))
-
-    result = ''
-    hlen = len(argv)
-    for i in range(hlen):
-        hvol = ord(argv[i])
-        hhex = '%02x' % hvol
-        result += hhex + ''
-    print(result)
-if __name__ == '__main__':
-    read_Com()
+com = serial.Serial('Com3',115200)
+bytea = com.read(40)
+str = bytea.hex()
+print (str)
