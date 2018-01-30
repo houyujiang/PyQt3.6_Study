3ba9d6117d1eafe5e6bc8439fe82719b69a30763
houyujiang <houyujiang@live.cn>
2018-01-30 16:25:13 +0800

    python3.6读取ID分组码坐标海拔

--- Com_Read_Test_py3.py
+++ Com_Read_Test_py3.py
@@ -25,27 +25,35 @@ def com_factory():
     #print ('读取到的字符长度：',len)
     str = com.read(40)
     str = str.hex()
-    print('读取到的结果',str)
+    #print('读取到的结果',str)
     return str
+
 #分解成两两一组的16进制字符
 def fen_jie_str(str):
     fen_jie_after = re.sub(r"(?<=\w)(?=(?:\w\w)+$)", " ", str)
     return fen_jie_after
+
 #id转换,16进制转换10进制
 def cover_id(str):
     id_orginal = str[6:12]  # 截取id
     id_cover = int(id_orginal,16)
     return id_cover
+
 def cover_fenzuma(str):
     fenzuma_orginal = str[18:22]  # 截取id
     fenzuma_cover = int(fenzuma_orginal, 16)
     return fenzuma_cover
+
 #转换昵称
 def cover_nicheng(str):
     pass
-#获取目标地址
+
+#获取目标地址(获取没任何意思)
 def cover_mubiao_dizhi(str):
-    pass
+    mubiaodizhi_org = str[12:18]
+    mubiaodizhi = int(mubiaodizhi_org,16)
+    return mubiaodizhi
+
 #转换经度坐标
 def cover_jindu(str):
     east_org = str[54:62]
@@ -55,6 +63,7 @@ def cover_jindu(str):
     east_num = int(east_str,16)
     east_zuobiao = east_num/float(1000000)
     return east_zuobiao
+
 #转换维度坐标
 def cover_weidu(str):
     north_org = str[64:72]
@@ -64,6 +73,7 @@ def cover_weidu(str):
     north_num = int(north_str, 16)
     north_zuobiao = north_num / float(1000000)
     return north_zuobiao
+
 #转换海拔高度
 def cover_haiba(str):
     haiba_org = str[72:76]
@@ -72,6 +82,7 @@ def cover_haiba(str):
     haiba_num = int(haiba_str,16)
     haiba_height = haiba_num/float(10)
     return haiba_height
+
 #读取设备状态
 def cover_state(str):
     pass
@@ -85,9 +96,11 @@ if __name__ == '__main__':
         east = cover_jindu(com_str)
         north = cover_weidu(com_str)
         haiba = cover_haiba(com_str)
+        mubiao = cover_mubiao_dizhi(com_str)
         print ('分解：',fenjie_str)
         print('id：',id)
         print('分组码：',fenzuma)
         print('东经：',east)
         print('维度',north)
         print('海拔高度',haiba)
+        print('目标地址：',mubiao)
